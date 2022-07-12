1bfc86b2ed802b7376bfe375cf3e8d1d9870e69d
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2022-07-12 20:09:02 +0800

    Migration (#98)
    * Add Alto theme
    
    * Remove unnecessary specificity from link hover selector
    
    * Add Dope theme
    
    * Refine Dope theme
    
    * Remove unnecessary selectors from dark mode styles
    
    * Add Ruby theme
    
    * Add branding+navigation header style
    
    * Post/page template cleanup for Ruby theme
    
    * Remove jQuery dependency from Ruby
    
    * Photoswipe for Ruby
    
    * package.json cleanup
    
    * Add Ease theme
    
    * Ease theme cleanup
    
    * Add Edge theme
    
    * Add callback to pagination script
    
    * Rebuild assets
    
    * Refresh Wave
    
    * Wave theme pagination
    
    * Remove unused settings
    
    * Add tag/author pages to Wave
    
    * Remove unnecessary dependencies
    
    * Post meta spacing
    
    * Dawn theme
    
    * Edition theme
    
    * Cleanup body class
    
    * Lint
    
    * More cleanup
    
    * London theme
    
    * Ease theme search cleanup
    
    * Minor bug fixes
    
    * Remove jQuery dependency from the custom search script
    
    * Put back commented functions
    
    * Update posts per page to make it consistent with homepage section posts
    
    * Alto theme update
    
    * Rebuild assets
    
    * Remove unused theme setting
    
    * Unnecessary rule
    
    * Start adding search support
    
    * Search support to more themes
    
    * Remove starter
    
    * Add Alto theme workflow

---
+++ assets/js/main.js
@@ -0,0 +1,135 @@
+$(function () {
+    'use strict';
+    feed();
+});
+
+function feed() {
+    'use strict';
+    var grid = document.querySelector('.post-feed');
+    var masonry;
+
+    imagesLoaded(grid, function () {
+        masonry = new Masonry(grid, {
+            itemSelector: '.grid-item',
+            columnWidth: '.grid-sizer',
+            stamp: '.related-title',
+            hiddenStyle: {transform: 'translateY(50px)', opacity: 0},
+            visibleStyle: {transform: 'translateY(0)', opacity: 1},
+        });
+
+        masonry.on('layoutComplete', function () {
+            grid.classList.add('initialized');
+        });
+
+        masonry.layout();
+
+        function callback(items) {
+            imagesLoaded(items, function (items) {
+                items.elements.forEach(function (item) {
+                    item.style.visibility = 'visible';
+                    masonry.appended(item);
+                    masonry.layout();
+                });
+            });
+        }
+
+        pagination(true, callback);
+    });
+
+    pswp(
+        '.post-feed',
+        '.grid-item:not(.grid-sizer)',
+        '.post-lightbox',
+        '.post-caption',
+        false
+    );
+}
+
+function pswp(container, element, trigger, caption, isGallery) {
+    var parseThumbnailElements = function (el) {
+        var items = [],
+            gridEl,
+            linkEl,
+            item;
+
+        $(el)
+            .find(element)
+            .each(function (i, v) {
+                gridEl = $(v);
+                linkEl = gridEl.find(trigger);
+
+                item = {
+                    src: isGallery
+                        ? gridEl.find('img').attr('src')
+                        : linkEl.attr('href'),
+                    w: 0,
+                    h: 0,
+                };
+
+                if (caption && gridEl.find(caption).length) {
+                    item.title = gridEl.find(caption).html();
+                }
+
+                items.push(item);
+            });
+
+        return items;
+    };
+
+    var openPhotoSwipe = function (index, galleryElement) {
+        var pswpElement = document.querySelectorAll('.pswp')[0],
+            gallery,
+            options,
+            items;
+
+        items = parseThumbnailElements(galleryElement);
+
+        options = {
+            closeOnScroll: false,
+            history: false,
+            index: index,
+            shareEl: false,
+            showAnimationDuration: 0,
+            showHideOpacity: true,
+        };
+
+        gallery = new PhotoSwipe(
+            pswpElement,
+            PhotoSwipeUI_Default,
+            items,
+            options
+        );
+        gallery.listen('gettingData', function (index, item) {
+            if (item.w < 1 || item.h < 1) {
+                // unknown size
+                var img = new Image();
+                img.onload = function () {
+                    // will get size after load
+                    item.w = this.width; // set image width
+                    item.h = this.height; // set image height
+                    gallery.updateSize(true); // reinit Items
+                };
+                img.src = item.src; // let's download image
+            }
+        });
+        gallery.init();
+    };
+
+    var onThumbnailsClick = function (e) {
+        e.preventDefault();
+
+        var index = $(e.target)
+            .closest(container)
+            .find(element)
+            .index($(e.target).closest(element));
+        var clickedGallery = $(e.target).closest(container);
+
+        openPhotoSwipe(index, clickedGallery[0]);
+
+        return false;
+    };
+
+    $(container).on('click', trigger, function (e) {
+        onThumbnailsClick(e);
+    });
+}
